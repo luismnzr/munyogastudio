c841652e2ca9294648bd7b2572ed1e740b9967a4
Luis Manzur <luismnzr@gmail.com>
2024-09-20 12:44:49 -0600

    url fix

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,8 +9,8 @@ class PaymentsController < ApplicationController
                   else
                     Stripe::Customer.create(email: current_user.email)
                   end,
-      success_url: 'https://www.munyogastudio.com/users/edit',
-      cancel_url: 'https://www.munyogastudio.com/payments/new',
+      success_url: 'hhttps://munyogastudio.com/users/edit',
+      cancel_url: 'https://munyogastudio.com/payments/new',
       line_items: [
         {price: params[:plan_id], quantity: 1},
       ],
