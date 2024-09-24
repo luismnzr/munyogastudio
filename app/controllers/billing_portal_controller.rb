c39f9ad2e16eda12dda339493a3f4f493cf34b3c
Luis Manzur <luismnzr@gmail.com>
2024-09-23 20:16:03 -0600

    billing portal 2 fix

--- app/controllers/billing_portal_controller.rb
+++ app/controllers/billing_portal_controller.rb
@@ -1,9 +1,9 @@
-class Stripe::BillingPortalController < ApplicationController
-    def create
-        session = Stripe::BillingPortal::Session.create({
-                customer: current_user.stripe_customer_id,
-                return_url: root_url
-            })
-        redirect_to session.url, allow_other_host: true
-    end
-end
+# class Stripe::BillingPortalController < ApplicationController
+#     def create
+#         session = Stripe::BillingPortal::Session.create({
+#                 customer: current_user.stripe_customer_id,
+#                 return_url: root_url
+#             })
+#         redirect_to session.url, allow_other_host: true
+#     end
+# end
